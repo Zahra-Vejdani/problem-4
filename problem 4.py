213946e5f2b968f2fac89c227cce54d293431921
Zahra-Vejdani <148383466+Zahra-Vejdani@users.noreply.github.com>
2023-10-23 23:36:57 +0330

    Add files via upload

---
+++ problem 4.py
@@ -0,0 +1,54 @@
+import math
+sin=math.sin
+cos=math.cos
+tan=math.tan
+fact=math.factorial
+sqr=math.sqrt
+num=int(input("How many times do you want the calculator to run?"))
+
+for i in range(num):
+    a=int(input("enter a: "))
+    re=input('please enter +, -, *, /, sin, cos, tan, cot, fact, sqr: ')
+
+    if re=="+":
+        b=float(input("enter b: "))
+        R=a+b
+
+    if re=="-":
+        b=float(input("enter b: "))
+        R=a-b
+
+    if re=="*":
+        b=float(input("enter b: "))
+        R=a*b
+
+    if re=="/":
+        b=float(input("enter b: "))
+        if b==0:
+            R="you cant enter zero for b :<"
+        else:
+            R=a/b
+
+    if re=="sin":
+        R=(math.sin(math.radians(a)))
+
+    if re=="cos":
+        R=(math.cos(math.radians(a)))
+
+    if re=="tan":
+            R=(math.tan(math.radians(a)))
+
+    if re=="cot":
+            z=(math.tan(math.radians(a)))
+            R=(1)/z
+
+    if re=="fact":
+        R=math.factorial(a)
+
+    if re=="sqr":
+        if a<0:
+            "We can`t do this"
+        else:
+            R=math.sqrt(a)
+
+    print(R)
